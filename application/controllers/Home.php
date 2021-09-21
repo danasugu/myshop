34654c768588cba93f339d07b87e934be21435ac
Dana Sugu <dana.sugu@gmail.com>
2021-09-21 12:36:31 +0300

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,7 +12,11 @@ class Home extends CI_Controller {
 
   public function aboutus()
 {
-  $this->load->view('aboutus');
+  $this->load->view('header/header');
+  $this->load->view('header/css');
+  $this->load->view('header/navbar');
+  $this->load->view('about/mainHome');
+  $this->load->view('header/footer');
 }
 
 }
---
+++ application/views/css.php
@@ -0,0 +1,4 @@
+
+//all css here
+
+</head>
