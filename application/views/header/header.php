9bec0f6b9ef011648201792335f27ea967be4a78
Dana Sugu <dana.sugu@gmail.com>
2021-09-24 19:07:37 +0300

    update

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -6,3 +6,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
   <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
