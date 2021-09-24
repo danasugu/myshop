57b41e6522214e40a4ae60df94b7fa4533f99470
Dana Sugu <dana.sugu@gmail.com>
2021-09-24 18:55:44 +0300

    update

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -5,4 +5,4 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <link rel="stylesheet" href="<?php echo base_url('assets/style.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
